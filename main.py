d1c07e87d757db55d59ebe656e8574e424054fdc
Asuroku <79782417+Asuroku@users.noreply.github.com>
2021-09-02 14:36:04 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -19,34 +19,62 @@ def get_message(message):
         bot.send_message(message.chat.id, f'Ок, {message.chat.first_name[5:3:-1]}{message.chat.first_name[3:]}, '
                                           f'давай сыграем.')
         bot.send_message(message.chat.id, 'Я загадал число в промежутке от 0 до 99. Сможешь угадать?')
-        a = randint(0, 99)
-        b = False
-        pop = 4
-        while b == False or pop != 0:
-            if message.text < 0 or message.int > 99:
-                pop -= 1
-                bot.send_message(message.chat.id, 'Я же сказал, от 0 до 9999!!!'
-                                                  f'У тебя осталось попыток: {pop})))')
-            elif message.text > a:
-                pop -= 1
-                bot.send_message(message.chat.id, 'Должен сказать, что моё число меньше')
-            elif message.text < a:
-                pop -= 1
-                bot.send_message(message.chat.id, 'Должен сказать, что моё число больше')
+        hidden_number = randint(0, 99)
+        guessed = False
+        attempts = 4
+
+        def request_page(message):
+            send = bot.send_message(message.chat.id, 'Введите страницу')
+            bot.register_next_step_handler(send, verify_page)
+
+
+        while guessed is False or attempts != 0:
+            if int(message.text.isdigit()):  # проверяем что введённое сообщение от пользователя является цифрой
+                guessed_number = int(message.text)
+                if guessed_number in range(0, 99):
+                    attempts -= 1
+                    bot.send_message(message.chat.id, 'Я же сказал, от 0 до 99!!!'
+                                                        f'У тебя осталось попыток: {attempts})))')
+                elif guessed_number > hidden_number:
+                    attempts -= 1
+                    bot.send_message(message.chat.id, 'Должен сказать, что моё число меньше')
+                elif guessed_number < hidden_number:
+                    attempts -= 1
+                    bot.send_message(message.chat.id, 'Должен сказать, что моё число больше')
+                else:
+                    bot.send_message(message.chat.id, f"That's right, object {message.chat.id}!")
             else:
-                bot.send_message(message.chat.id, f"That's right, object {message.chat.id}!")
-                bot.send_message(message.chat.id, 'ERROR')
-                sleep(3)
-                bot.send_message(message.chat.id, 'ERROR')
-                sleep(3)
-                bot.send_message(message.chat.id, 'ERROR')
-                sleep(3)
-                bot.send_message(message.chat.id, 'REBOOTING THE ENTIRE SYSTEM')
-                sleep(10)
-                bot.send_message(message.chat.id, 'RELOADING LANGUAGE PACKS')
-                sleep(6)
-                bot.send_message(message.chat.id, 'ПЕРЕЗАГРУЗКА ЗАВЕРШЕНА')
-                bot.send_message(message.chat.id, 'Я Asuroku, и я бот! Чем могу быть полезен?')
+                bot.send_message(message.chat.id, 'Номер страницы должен быть числом')
+                get_message(message)
+#            try:
+#               guessed_number = int(message.text)
+#            except TypeError:
+#                bot.send_message(message.chat.id, 'Не понимаю. Введи число.')
+#                continue
+#            if guessed_number < 0 or guessed_number > 99:
+#                attempts -= 1
+#                bot.send_message(message.chat.id, 'Я же сказал, от 0 до 9999!!!'
+#                                                    f'У тебя осталось попыток: {attempts})))')
+#            elif guessed_number > hidden_number:
+#                attempts -= 1
+#                bot.send_message(message.chat.id, 'Должен сказать, что моё число меньше')
+#            elif guessed_number < hidden_number:
+#                attempts -= 1
+#                bot.send_message(message.chat.id, 'Должен сказать, что моё число больше')
+#            else:
+#                bot.send_message(message.chat.id, f"That's right, object {message.chat.id}!")
+#                bot.send_message(message.chat.id, 'ERROR')
+#                sleep(3)
+#                bot.send_message(message.chat.id, 'ERROR')
+#                sleep(3)
+#                bot.send_message(message.chat.id, 'ERROR')
+#                sleep(3)
+#                bot.send_message(message.chat.id, 'REBOOTING THE ENTIRE SYSTEM')
+#                sleep(10)
+#                bot.send_message(message.chat.id, 'RELOADING LANGUAGE PACKS')
+#                sleep(6)
+#                bot.send_message(message.chat.id, 'ПЕРЕЗАГРУЗКА ЗАВЕРШЕНА')
+#                bot.send_message(message.chat.id, 'Я Asuroku, и я бот! Чем могу быть полезен?')
     else:
         bot.send_message(message.chat.id, f'{message.text[::-1]}')
 
